220b91828f45f0ce7f9a1bf595029ae8f621356f
Chris Speakes <68981729+13alvone@users.noreply.github.com>
2023-07-02 07:59:53 -0700

    Create uninstall.sh
    Staging initial uninstaller for quick removal.

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+set -x
+
+if [ "$(id -u)" = "0" ]; then
+    echo "[x] This script should not be started with with SUDO but will request it for a few subcommands.." >&2
+    exit 1
+fi
+
+user_home=$(eval echo $HOME)
+source_user=$(eval whoami)
+
+echo "[i] Requesting SUDO to remove binary in production: /usr/local/bin/ and $HOME/.ctee/ directories.\n"
+sudo rm -rf $HOME/.ctee/
+sudo rm -rf /usr/local/bin/ctee
