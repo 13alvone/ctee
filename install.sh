35c123bc3cae7615682848d6e2872c8220c5f3ed
13alvone <13alvone@gmail.com>
2023-07-02 09:57:03 -0700

    ** Major overhaul, reverted back to bash shell given all of the complexities of zsh, and script's inability to record colors properly, rather it records the control characters that make the colors in interactive zsh's and it's therefore very hard to read/descipher the real commands from the color control patterns.

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
-#!/bin/zsh
+#!/bin/bash
 
 if [ "$(id -u)" = "0" ]; then
     echo "[x] This script should not be started with with SUDO but will request it for a few subcommands.." >&2
     exit 1
 fi
 
+script_dir=$(dirname "$0")
 user_home=$(eval echo $HOME)
 source_user=$(eval whoami)
 
 mkdir -p $HOME/.ctee/etc/
-cp zshrc.ctee /home/$source_user/.ctee/etc/
-chmod +x /home/$source_user/.ctee/etc/zshrc.ctee
-echo "[+] Requesting SUDO to place binary in production: /usr/local/bin/\n"
+echo "[i] Requesting SUDO to place binary in production: /usr/local/bin/\n"
 sudo cp ctee /usr/local/bin/
 sudo chmod +x /usr/local/bin/ctee
 sudo chown -R $source_user:$source_user /home/$source_user/.ctee/ /usr/local/bin/ctee
-
+cp bashrc.ctee $HOME/.ctee/etc/
+chmod +x $HOME/.ctee/etc/bashrc.ctee
+echo "[+] Succesfully installed the bash version of ctee! 'ctee' can now be called from anywhere in the terminal to start command-line recording!\n"
--- uninstall.sh
+++ uninstall.sh
@@ -11,3 +11,4 @@ source_user=$(eval whoami)
 echo "[i] Requesting SUDO to remove binary in production: /usr/local/bin/ and $HOME/.ctee/ directories.\n"
 sudo rm -rf $HOME/.ctee/
 sudo rm -rf /usr/local/bin/ctee
+echo "[+] Removed the following components successfully and fully uninstalled CTEE:\n\t[-] Removed: $HOME/.ctee/\n\t[-] Removed: /usr/loca/bin/ctee\n"
