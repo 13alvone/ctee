726dca1ffd116d55df748c09e1adc2dec30676c9
13alvone <13alvone@gmail.com>
2023-07-04 01:06:35 -0700

    ** Added the new cxrun cli tool that allows you to replay the most recently recorded ctee shell session at a modified speed 'default is to reduce by 50'

--- install.sh
+++ install.sh
@@ -11,9 +11,12 @@ source_user=$(eval whoami)
 
 mkdir -p $HOME/.ctee/etc/
 echo "[i] Requesting SUDO to place binary in production: /usr/local/bin/\n"
-sudo cp ctee cxwrite zero_timing /usr/local/bin/
-sudo chmod +x /usr/local/bin/ctee /usr/local/bin/cxwrite /usr/local/bin/zero_timing
-sudo chown -R $source_user:$source_user /home/$source_user/.ctee/ /usr/local/bin/ctee /usr/local/bin/cxwrite /usr/local/bin/zero_timing
+sudo cp ctee cxwrite cxrun zero_timing /usr/local/bin/
+sudo chmod +x /usr/local/bin/ctee /usr/local/bin/cxwrite /usr/local/bin/zero_timing /usr/local/bin/cxrun
+sudo chown -R $source_user:$source_user /home/$source_user/.ctee/ /usr/local/bin/ctee /usr/local/bin/cxwrite /usr/local/bin/zero_timing /usr/local/bin/cxrun
 cp bashrc.ctee $HOME/.ctee/etc/
 chmod +x $HOME/.ctee/etc/bashrc.ctee
 echo "[+] Succesfully installed the bash version of ctee! 'ctee' can now be called from anywhere in the terminal to start command-line recording!\n"
+echo "[+] Components added to your \$PATH via default location '/usr/local/bin/':"
+echo "    [-] ctee --> A powerful pseudoshell with full functionality that records a local shell session."
+echo "    [-] cxrun --> A quick command to rerun the most recently recorded session at an increased speed."
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 if [ "$(id -u)" = "0" ]; then
-    echo "[x] This script should not be started with with SUDO but will request it for a few subcommands.." >&2
+    echo "[x] This script should not be started with SUDO but will request it for a few subcommands.." >&2
     exit 1
 fi
 
@@ -10,14 +10,30 @@ source_user=$(eval whoami)
 
 # Moving the cxwrite_notes.db to /tmp and renaming to make it clear that this is a temporary backup of the notes database.
 # This provides small grace period for users to preserve their notes.db after uninstall before they are deleted by the system."
-mv $HOME/.ctee/cxwrite_notes.db /tmp/BACKUP_cxwrite_notes.db
-sudo chown -R root:root /tmp/BACKUP_cxwrite_notes.db
-sudo chmod -R 755 /tmp/BACKUP_cxwrite_notes.db
 
+db_file="$HOME/.ctee/cxwrite_notes.db"
+backup_file="/tmp/BACKUP_cxwrite_notes.db"
+
+if [[ -f "$db_file" ]]; then
+    mv "$db_file" "$backup_file"
+    if [[ -f "$backup_file" ]]; then
+        sudo chown -R root:root "$backup_file"
+        sudo chmod -R 755 "$backup_file"
+        echo "[+] Backup file was created successfully: $backup_file"
+    else
+        exit 1
+    fi
+else
+    exit 1
+fi
 
 echo "[i] Requesting SUDO to remove binary in production: /usr/local/bin/ and $HOME/.ctee/ directories.\n"
 sudo rm -rf $HOME/.ctee/
-sudo rm -rf /usr/local/bin/ctee /usr/local/bin/cxwrite /usr/local/bin/zero_timing
+sudo rm -rf /usr/local/bin/ctee
+sudo rm -rf /usr/local/bin/cxwrite
+sudo rm -rf /usr/local/bin/zero_timing
+sudo rm -rf /usr/local/bin/cxrun 
 echo "[+] Removed the following components successfully and fully uninstalled CTEE:"
-echo "\n\t[-] Removed: $HOME/.ctee/\n\t[-] Removed: /usr/loca/bin/ctee\n\t[-] Removed: /usr/local/bin/cxwrite\n"
+echo "\n\t[-] Removed: $HOME/.ctee/\n\t[-] Removed: /usr/local/bin/ctee"
+echo "\n\t[-] Removed: /usr/local/bin/cxwrite\n\t[-] Removed: /usr/local/bin/cxrun"
 
