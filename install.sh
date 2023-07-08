fc26e64d6f55704fe4fdeeb103ae9fa3395b99b3
13alvone <13alvone@gmail.com>
2023-07-08 10:54:32 -0700

    ** Updated code to move the ALL resulting components related to ctee into $HOME/.ctee/. ** Updated CTEE to check for updates each time it is run, and gracefully auto-update in the background if so. ** Updated ctee to output a copy of the last ran ctee recording to the current working directory as well as store itself, as normal, in $HOME/.ctee/ for backup, however, the days allowed to exist has been changed to 1, given the copy that the user is also provided within their current working directory.

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ user_home=$(eval echo $HOME)
 source_user=$(eval whoami)
 
 mkdir -p $HOME/.ctee/etc/
-echo "[i] Requesting SUDO to place binary in production: /usr/local/bin/\n"
+mkdir -p $HOME/.ctee/git/
+echo "[i] Requesting SUDO to place binary in production: /usr/local/bin/"
 sudo cp ctee cxwrite cxrun cxtiming cxrmclear /usr/local/bin/
 sudo chmod +x /usr/local/bin/ctee /usr/local/bin/cxwrite /usr/local/bin/cxtiming /usr/local/bin/cxrun /usr/local/bin/cxrmclear
 
@@ -29,9 +30,13 @@ sudo chown -R $source_user:$source_group $HOME/.ctee/ /usr/local/bin/ctee /usr/l
 sudo chown -R $source_user:$source_group /usr/local/bin/cxtiming /usr/local/bin/cxrun /usr/local/bin/cxrmclear
 cp bashrc.ctee $HOME/.ctee/etc/
 chmod +x $HOME/.ctee/etc/bashrc.ctee
-echo "[+] Succesfully installed the bash version of ctee! 'ctee' can now be called from anywhere in the terminal to start command-line recording!\n"
+echo "[+] Succesfully installed the bash version of ctee! 'ctee' can now be called from anywhere in the terminal to start command-line recording!"
 echo "[+] Components added to your \$PATH via default location '/usr/local/bin/':"
 echo "    [-] ctee --> A powerful pseudoshell with full functionality that records a local shell session."
 echo "    [-] cxrun --> A quick command to rerun the most recently recorded session at an increased speed."
 echo "    [-] cxtiming --> A quick script that adjusts and/or normalizes the timing file of a given script recording."
 echo "    [-] cxrmclear --> A quick script that removes several control sequences for easier task output."
+echo "    [-] cxwrite --> A quick script that writes notes via CLI during a ctee session."
+
+mv $HOME/ctee/ $HOME/.ctee/git/
+
--- update.sh
+++ update.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-./uninstall.sh
-./install.sh
+current_dir=$(pwd)
+cd $HOME/.ctee/git/ctee/
+# Fetch updates from the remote repository
+git fetch origin
+
+# Compare the local and remote repositories
+git diff --quiet origin/master
+
+# Check the exit status of the diff command.
+# If there are no changes, the exit status will be 0, so the if-statement will run.
+if [ $? -eq 0 ]; then
+    echo "[i] CTEE Verfied Up-To-Date. Continuing..."
+else
+    echo "[+] Remote Github changes were detected. Running CTEE Updater..."
+    cd $HOME
+    git clone https://github.com/13alvone/ctee.git
+    cd $HOME/ctee
+    ./install.sh
+fi
+
+cd $current_dir
