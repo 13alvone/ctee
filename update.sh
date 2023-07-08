6c8fc74ec08cdff80b9618d48c44ed2b2ab4f669
13alvone <13alvone@gmail.com>
2023-07-08 11:11:25 -0700

    ** Updated code to move the ALL resulting components related to ctee into $HOME/.ctee/. ** Updated CTEE to check for updates each time it is run, and gracefully auto-update in the background if so. ** Updated ctee to output a copy of the last ran ctee recording to the current working directory as well as store itself, as normal, in $HOME/.ctee/ for backup, however, the days allowed to exist has been changed to 1, given the copy that the user is also provided within their current working directory.

--- update.sh
+++ update.sh
@@ -1,25 +1,51 @@
 #!/usr/bin/env bash
 
+# Save the current directory
 current_dir=$(pwd)
-cd $HOME/.ctee/git/ctee/
+
+# Change to the repository directory
+cd "$HOME/.ctee/git/ctee/" || { kill -s TERM $$; exit 1; }
+
 # Fetch updates from the remote repository
 git fetch origin
 
-# Compare the local and remote repositories
-git diff --quiet origin/master
+# Get the current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Compare the local branch with the remote branch
+git diff --quiet origin/"$current_branch"
 
 # Check the exit status of the diff command.
 # If there are no changes, the exit status will be 0, so the if-statement will run.
 if [ $? -eq 0 ]; then
-    echo "[i] CTEE Verfied Up-To-Date. Continuing..."
+    echo "[i] CTEE Verified Up-To-Date. Continuing..."
 else
     echo "[+] Remote Github changes were detected. Running CTEE Updater..."
-    cd $HOME
+
+    # Change back to HOME directory
+    cd "$HOME" || { kill -s TERM $$; exit 1; }
+
+    # Remove the old repository before cloning
+    rm -rf ctee
+
+    # Clone the repository
     git clone https://github.com/13alvone/ctee.git
-    cd $HOME/ctee
+
+    # Change to the new repository directory
+    cd ctee || { kill -s TERM $$; exit 1; }
+
+    # Run the install script
     ./install.sh
+
     echo "[+] CTEE updated. Please run ctee again for updated version..."
-    exit 1
+
+    # Return to the original directory
+    cd "$current_dir" || { kill -s TERM $$; exit 1; }
+
+    # Exit with status 1 to indicate that the script didn't complete normally
+    kill -s TERM $$; exit 1
 fi
 
-cd $current_dir
+# Return to the original directory
+cd "$current_dir" || { kill -s TERM $$; exit 1; }
+
